8395af3253b9d98cde55b58cb5577a04d5d14755
Wouter Van Schandevijl <woutervs@hotmail.com>
2026-03-28 15:22:57 +0100

    we're going to add a level 0

---
+++ levels/level-0/zombie.py
@@ -0,0 +1,24 @@
+# Need to evaluate the need of the comments in the code
+# There already is uitleg.md
+# Maybe the code needs to be simplified
+# Level 0 challenges --> work towards level 1
+# Add thread sleeps, randomtints + ifs etc
+
+print("🧟‍♂️💀 WELKOM BIJ ZOMBIE APOCALYPSE 💀🧟‍♂️")
+print()
+print("🌫️ Het is donker... je hoort gegrom...")
+print("🧟‍♂️ Er komt een zombie op je af!")
+print()
+
+actie = input("⚡ Wat doe je? (rennen / vechten) ➜ ")
+
+if actie == "rennen":
+    print("💨🔥 Je bent ontsnapt! Je hart bonkt in je keel...")
+elif actie == "vechten":
+    print("💥🧟‍♂️ BOEM! Je verslaat de zombie als een held!")
+else:
+    print("🤦 Zombies twijfelen niet...")
+    print("☠️ Je bent dood.")
+
+print()
+print("🎬 THE END 🎬")
